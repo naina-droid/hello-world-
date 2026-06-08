f330f1ba20d7e652eff992afb632aeb91a11e4cd
Naina Das <nainadad985@gmail.com>
2026-06-08 17:11:51 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 
+# Amar prothom Python code
+name = "Naina Das"
+university = "Leading University"
+
+print("Hello World!")
+print("My name is", name)
+print("I study at", university)
+print("Future AI Engineer loading...")
